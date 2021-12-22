fa1990597945d75492e1e3d6908a99d0b73f85af
Deki97 <dejan97@libero.it>
2021-12-22 18:56:32 +0100

    Animated Loader done

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -2,8 +2,6 @@
     <div class="loader">
         <div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
     </div>
-
-    
 </template>
 
 
